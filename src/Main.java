827335d4dcf2fb53293f6d417e1e16be171db09e
ruaidhri13 <ruaidhri2006@gmail.com>
2025-04-16 11:51:34 +0100

    Base calc functioning correctly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int litres;
+        String fuelType;
+        double dieselCostPerLitre = 1.54;
+        double petrolCostPerLitre = 1.37;
+        double totalFuelPrice;
+        boolean isValidFuelType = false;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Diesel: £" + dieselCostPerLitre +
+                            " Petrol: £" + petrolCostPerLitre + "\n"); // Displays the price of each fuel type
+
+        while (!isValidFuelType) {
+            System.out.println("Diesel or Petrol");
+            fuelType = scanner.nextLine();
+
+            if (fuelType.equalsIgnoreCase("Diesel") || fuelType.equalsIgnoreCase("Petrol")) {
+                isValidFuelType = true;
+            }
+            else
+                System.out.println("Invalid fuel type");
+        }
+
+
+        System.out.println("Enter the number of litres you want to insert: ");
+
+        litres = scanner.nextInt();
+
+        totalFuelPrice = Math.round(litres * petrolCostPerLitre);
+
+        System.out.println("Total Cost: £" + totalFuelPrice);
+
+
         System.out.println("Fuel Calculator");
     }
 }
